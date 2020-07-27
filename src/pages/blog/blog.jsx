3bc410418180ced380dcb36ec7706326a6c83a90
Elton Zheng <elton.zheng.sh@gmail.com>
2020-07-27 18:03:34 +0800

    render a list.

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -10,21 +10,36 @@ function Blog() {
     { id: 4, name: "香秀" },
   ];
   const [blogTitle, setBlogTitle] = useState("elton zheng");
+  const [articleList, setArticleList] = useState([]);
 
   const gotoIndex = () => {
     Taro.navigateTo({ url: "/pages/index/index?blogTitle=" + blogTitle });
+  };
+
+  const getBlogList = () => {
+    Taro.request({
+      url: "https://apiblog.jspang.com/default/getArticleList",
+    }).then((res) => {
+      console.log(res.data);
+      setArticleList(res.data.list);
+    });
   };
 
   return (
     <View>
       <Text>Blog Page</Text>
       <Button onClick={gotoIndex}>返回首页</Button>
+      <Button onClick={getBlogList}>获取博客列表</Button>
       {girls.map((item, index) => {
         return (
           <View key={index}>
             {item.id}:{item.name}
           </View>
         );
+      })}
+
+      {articleList.map((item, index) => {
+        return <View key={index}>- {item.title}</View>;
       })}
     </View>
   );
